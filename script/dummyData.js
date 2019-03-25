5c730f694bac40765dc30b5507fc062d592e1c07
Andrew Myint <doku.potharhtoo@gmail.com>
2019-03-25 00:34:03 -0400

    validation error on seeding

--- script/dummyData.js
+++ script/dummyData.js
@@ -1,3 +1,40 @@
+const faker = require('faker');
+
+var generateBenefitType = () => {
+  const type = ['Anti-inflammatory', 'Beautifying', 'Cleansing', 'Digestive Support',
+                'Energizing','Immunity Boosting', 'Stress Relief', 'Workout Recovery'];
+  let random = Math.floor(Math.random() * 8);
+  return type[random];
+}
+// mock data for Benefits Model(table)
+var mockBenefits = function(){
+  let tempArray = [];
+  for (var i = 0; i < 100; i++) {
+    tempArray.push({
+      benefitType: generateBenefitType(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+  }
+  return tempArray;
+}
+// console.log(faker.commerce.productAdjective());
+
+var mockCurrentItem = () => {
+  let tempArray = [];
+  for (var i = 0; i < 100; i++) {
+    tempArray.push({
+      benefitType: faker.commerce.product(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+  }
+  return tempArray;
+}
+// console.log(mockCurrentItem());
+
+module.exports.mockBenefits = mockBenefits;
+
 /*
   keyBenefits
 
@@ -10,4 +47,6 @@
   Stress Relief
   Workout Recovery
 */
+
+
 
---
+++ seeders/20190325041038-benefits-seeder.js
@@ -0,0 +1,30 @@
+'use strict';
+const {mockBenefits} = require('../script/dummyData');
+console.log(mockBenefits());
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   let mockData = mockBenefits();
+   return queryInterface.bulkInsert('benefits', mockData, {});
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
