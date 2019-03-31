7f117d9ab85e4fb90a6a33c32ef458f76501f02e
Andrew Myint <doku.potharhtoo@gmail.com>
2019-03-31 01:51:00 -0400

    Created template for crousel service

--- client/app.jsx
+++ client/app.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import NavBar from './component/nav.jsx';
+import Crousel from './component/crousel.jsx';
+import Benefits from './component/benefits.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      benefits: ['Anti-inflammatory', 'Beautifying', 'Cleansing', 'Digestive Support'],
+      images: ['https://s3.amazonaws.com/dailyharvest.images/images/image-1ncreased-1Shk_PkNkNw.jpg',
+        'https://s3.amazonaws.com/dailyharvest.images/images/image-alexischloe-tDcOX3rOC_M.jpg',
+        'https://s3.amazonaws.com/dailyharvest.images/images/image-christine_siracusa-bypcCMcIoVE.jpg']
     }
   }
   render() {
+    console.log(window.location);
     return (
       <div>
-        <div>Hello from React</div>
+        <div className="position-relative"><NavBar /></div>
+        <div className="container">
+          <h6>home / smoothie / ginger+greens</h6>
+          <table>
+            <tbody>
+              <tr>
+                <td><div className="itemContainer"><Crousel images={this.state.images} /></div></td>
+                <td><div className="itemDetail">Hellooooooooooooo</div></td>
+              </tr>
+              <tr>
+                <td>blank</td>
+                <td><Benefits benefits={this.state.benefits} /></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
+
     )
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react'],
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
       }
     ]
   }
